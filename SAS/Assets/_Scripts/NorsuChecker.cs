599d35c6bb0089011be0c5658125dd5d85168338
RiikkaKilp <32130006+RiikkaKilp@users.noreply.github.com>
2020-01-16 09:28:23 +0200

    norsu check

---
+++ SAS/Assets/_Scripts/NorsuChecker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NorsuChecker : MonoBehaviour
+{
+    Vector3 defaultPosition;
+
+    void Start()
+    {
+        defaultPosition = transform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.otherCollider.tag == "Norsu")
+        {
+            transform.position = new Vector3(defaultPosition.x, defaultPosition.y - 0.1f, defaultPosition.z);
+            
+        }            
+    }
+}
